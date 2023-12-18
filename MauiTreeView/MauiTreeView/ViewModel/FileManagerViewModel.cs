e5022f9d847f87573b7fecae475b7523cfc99f02
DiwakarVenkatesan <diwakar.venkatesan@syncfusion.com>
2023-12-18 16:51:36 +0530

    Added sample and README.md

---
+++ MauiTreeView/MauiTreeView/ViewModel/FileManagerViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiTreeView
+{
+    public class FileManagerViewModel
+    {
+        public ObservableCollection<Folder> Folders { get; set; }
+
+        public FileManagerViewModel()
+        {
+            this.Folders = GetFiles();
+        }
+
+        private ObservableCollection<Folder> GetFiles()
+        {
+            var nodeImageInfo = new ObservableCollection<Folder>();
+
+            var doc = new Folder() { ItemName = "Documents", ImageIcon = "folder.png" };
+            var download = new Folder() { ItemName = "Downloads", ImageIcon = "folder.png" };
+            var mp3 = new Folder() { ItemName = "Music", ImageIcon = "folder.png" };
+            var pictures = new Folder() { ItemName = "Pictures", ImageIcon = "folder.png" };
+            var video = new Folder() { ItemName = "Videos", ImageIcon = "folder.png" };
+
+            var pollution = new File() { ItemName = "Environmental Pollution.docx", ImageIcon = "word.png" };
+            var globalWarming = new File() { ItemName = "Global Warming.ppt", ImageIcon = "ppt.png" };
+            var sanitation = new File() { ItemName = "Sanitation.docx", ImageIcon = "word.png" };
+            var socialNetwork = new File() { ItemName = "Social Network.pdf", ImageIcon = "pdfimage.png" };
+            var youthEmpower = new File() { ItemName = "Youth Empowerment.pdf", ImageIcon = "pdfimage.png" };
+
+            var tutorials = new File() { ItemName = "Tutorials.zip", ImageIcon = "zip.png" };
+            var typeScript = new File() { ItemName = "TypeScript.7z", ImageIcon = "zip.png" };
+            var uiGuide = new File() { ItemName = "UI-Guide.pdf", ImageIcon = "pdfimage.png" };
+
+            var song = new File() { ItemName = "Gouttes", ImageIcon = "audio.png" };
+
+            var camera = new File() { ItemName = "Camera Roll", ImageIcon = "folder.png" };
+            var stone = new File() { ItemName = "Stone.jpg", ImageIcon = "image.png" };
+            var wind = new File() { ItemName = "Wind.jpg", ImageIcon = "image.png" };
+
+            var img0 = new File() { ItemName = "WIN_20160726_094117.JPG", ImageIcon = "people_circle23.png" };
+            var img1 = new File() { ItemName = "WIN_20160726_094118.JPG", ImageIcon = "people_circle2.png" };
+
+            var video1 = new File() { ItemName = "Naturals.mp4", ImageIcon = "video.png" };
+            var video2 = new File() { ItemName = "Wild.mpeg", ImageIcon = "video.png" };
+
+            doc.Files = new ObservableCollection<File>
+            {
+                pollution,
+                globalWarming,
+                sanitation,
+                socialNetwork,
+                youthEmpower
+            };
+
+            download.Files = new ObservableCollection<File>
+            {
+                tutorials,
+                typeScript,
+                uiGuide
+            };
+
+            mp3.Files = new ObservableCollection<File>
+            {
+                song
+            };
+
+            pictures.Files = new ObservableCollection<File>
+            {
+                camera,
+                stone,
+                wind
+            };
+
+            camera.SubFiles = new ObservableCollection<File>
+            {
+                img0,
+                img1
+            };
+
+            video.Files = new ObservableCollection<File>
+            {
+                video1,
+                video2
+            };
+
+            nodeImageInfo.Add(doc);
+            nodeImageInfo.Add(download);
+            nodeImageInfo.Add(mp3);
+            nodeImageInfo.Add(pictures);
+            nodeImageInfo.Add(video);
+            return nodeImageInfo;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # how-to-hide-expander-icon-.net-maui-treeview
 This example demonstrates about how to hide the expander icon in TreeViewNode in .NET MAUI TreeView (SfTreeView).
+
+In [.NET MAUI TreeView](https://www.syncfusion.com/maui-controls/maui-treeview), the ability to expand and collapse nodes. However, there may be scenarios where you want to remove expander icon expand/collapse functionality. This can be achieved by hiding the expander of the TreeView nodes. Below is the article on how to hide the expander in .NET MAUI TreeView.
+
+**XAML**
+ 
+```xml
+<syncfusion:SfTreeView x:Name="treeView"
+                           ExpandActionTarget="Node"
+                           ExpanderWidth="0"
+                           ItemsSource="{Binding Folders}"
+                           ChildPropertyName="Files">
+        
+        <syncfusion:SfTreeView.ItemTemplate>
+            <DataTemplate>
+                <Grid x:Name="grid"
+                      RowSpacing="0"
+                      ColumnDefinitions="40, *"
+                      BackgroundColor="Transparent">
+                    <Grid Padding="5,5,5,5">
+                        <Image Source="{Binding ImageIcon}"
+                               VerticalOptions="Center"
+                               HorizontalOptions="Center"
+                               HeightRequest="35"
+                               WidthRequest="35" />
+                    </Grid>
+                    <Grid Grid.Column="1"
+                          RowSpacing="1"
+                          Padding="1,0,0,0"
+                          VerticalOptions="Center">
+                        <Label LineBreakMode="NoWrap"
+                               Text="{Binding ItemName}"
+                               VerticalTextAlignment="Center" />
+                    </Grid>
+                </Grid>
+            </DataTemplate>
+        </syncfusion:SfTreeView.ItemTemplate>
+    </syncfusion:SfTreeView>
+
+ ```
+
+[View sample in GitHub](https://github.com/SyncfusionExamples/how-to-hide-expander-icon-.net-maui-treeview)
+
+**Conclusion**
+I hope you enjoyed learning about How to hide expander in .NET MAUI TreeView.
+You can refer to our [.NET MAUI TreeView](https://www.syncfusion.com/maui-controls/maui-treeview) feature tour page to know about its other groundbreaking feature representations and documentation, and how to quickly get started for configuration specifications. You can also explore our .NET MAUI TreeView example to understand how to create and present data. For current customers, you can check out our components from the License and Downloads page. If you are new to Syncfusion, you can try our 30-day free trial to check out our other controls.
+
+If you have any queries or require clarifications, please let us know in the comments section below. You can also contact us through our support forums, Direct-Trac, or feedback portal. We are always happy to assist you!
